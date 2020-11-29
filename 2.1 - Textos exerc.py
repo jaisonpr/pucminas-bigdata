246dfa761f4577edff1c3a6b297481d5c0f5b715
Jaison PR <jaisonpr@gmail.com>
2020-11-29 12:55:46 -0300

    Initial

---
+++ 2.1 - Textos exerc.py
@@ -0,0 +1,41 @@
+import mincemeat
+import csv
+import glob
+
+text_files = glob.glob('./2.1 - textos/*')
+
+def file_contents(file_name):
+    f = open(file_name)
+    try:
+        return f.read()
+    finally:
+        f.close()
+        
+source = dict((file_name, file_contents(file_name))for file_name in text_files)
+
+def mapfn(k, v):
+    print ('map' + k)
+    from stopwords import allStopWords
+    for line in v.splitlines():
+        for word in line.split():
+            if (word not in allStopWords ):
+                yield word, 1
+
+def reducefn(k, v):
+    print ('reduce ' + k)
+    return sum(v)
+
+
+s = mincemeat.Server()
+
+# The data source can be any dictionary-like object
+s.datasource = source
+s.mapfn = mapfn
+s.reducefn = reducefn
+
+results = s.run_server(password="changeme")
+#print results
+
+w = csv.writer(open("./2.1-RESULT.csv", "w"))
+for k, v in results.items():
+    w.writerow([k, v])
